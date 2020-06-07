162feeda9f3316b09add9f201ee12e78fc4437e3
Richard Rocca <rdrh29@gmail.com>
2020-06-07 01:00:46 -0500

    Updates

---
+++ precache-manifest.ffa245c9f6246782a4f899e125a95907.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcef2e79e08cc8d9d9112c907d600aa4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9ce39e4ada84ab72a03f",
+    "url": "/monsters-rolodex/static/css/main.64e6c62e.chunk.css"
+  },
+  {
+    "revision": "af47915f51a8cf03e878",
+    "url": "/monsters-rolodex/static/js/2.a8286f47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a8286f47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ce39e4ada84ab72a03f",
+    "url": "/monsters-rolodex/static/js/main.c872b804.chunk.js"
+  },
+  {
+    "revision": "1e2779be45af9a021e71",
+    "url": "/monsters-rolodex/static/js/runtime-main.101989cd.js"
+  }
+]);
